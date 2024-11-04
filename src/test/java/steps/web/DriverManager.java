1af3fed9e1f30c2a182f4e7b40f48e4eb8304e91
alwan wirawan <alwan.wirawan@djembatandua.com>
2024-11-04 09:17:25 +0700

    feat: update driver manager

--- src/test/java/steps/web/DriverManager.java
+++ src/test/java/steps/web/DriverManager.java
@@ -14,7 +14,7 @@ public class DriverManager {
 
     private static WebDriver driver;
     private static WebDriverWait wait;
-    private static final String SELENIUM_GRID_URL = "http://localhost:4444/wd/hub";
+    private static final String SELENIUM_GRID_URL = "http://172.19.0.2:4444/wd/hub";
     private static final String ENV = System.getProperty("ENV", "local"); // Default to local if not specified
 
     public static void initializeDriver() {
